9ef4dbef2da178dcbf4f84f9a4df380dd80a4c02
FarzeenKist <farzeenkistnareddy@gmail.com>
2023-08-15 17:08:07 +0400

    fix package.json, project's structure and add improvements

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,13 @@ type Vehicle = Record<{
     createdAt: nat64;
 }>
 
+type VehiclePayload = Record<{
+    model: string;
+    version: string;
+    mileage: nat64;
+    depreciation: nat64;
+}>
+
 type MileagePayload = Record<{
     miles: nat64;
 }>
@@ -27,6 +34,13 @@ export function getVehicle(id: string): Result<Vehicle, string> {
         Some: (Vehicle) => Result.Ok<Vehicle, string>(Vehicle),
         None: () => Result.Err<Vehicle, string>(`a vehicle with id=${id} not found`)
     });
+}
+
+$update
+export function createVehicle(payload: VehiclePayload) : Result<Vehicle, string>{
+    const vehicle: Vehicle = {...payload, id: uuidv4(), createdAt: ic.time()}
+    vehicleStorage.insert(vehicle.id, vehicle);
+    return Result.Ok(vehicle);
 }
 
 $update;
@@ -47,7 +61,7 @@ export function addMileage(id: string, payload: MileagePayload): Result<Vehicle,
 
 
 $update;
-export function calculateDepreciation(id: string, payload: DepreciationPayload): Result<Vehicle, string> {
+export function updateDepreciation(id: string, payload: DepreciationPayload): Result<Vehicle, string> {
     return match(vehicleStorage.get(id), {
         Some: (Vehicle) => {
             const updatedVehicle: Vehicle = {
@@ -60,4 +74,19 @@ export function calculateDepreciation(id: string, payload: DepreciationPayload):
         },
         None: () => Result.Err<Vehicle, string>(`a vehicle with id=${id} not found`)
     });
+}
+
+
+
+// a workaround to make uuid package work with Azle
+globalThis.crypto = {
+    getRandomValues: () => {
+        let array = new Uint8Array(32)
+
+        for (let i = 0; i < array.length; i++) {
+            array[i] = Math.floor(Math.random() * 256)
+        }
+
+        return array
+    }
 }
